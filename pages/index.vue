3ef60de6440588f18d2edcf80ead6825ffff6f66
sam.liu <sam.liu@axis3d.com>
2023-01-12 19:01:54 +0800

    feat: 新增首頁
    1. 建立全局body style

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    PIXELIGHT
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Noto+Sans+TC:wght@300;400;500&display=swap');
+
+body {
+    font-family: 'Montserrat', 'Noto Sans TC', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #F5F5F5;
+    margin: 0px;
+}
+</style>
