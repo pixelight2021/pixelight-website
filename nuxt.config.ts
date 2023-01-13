71f932807360afab39bd2b55feff3b8e62740c54
sam.liu <sam.liu@axis3d.com>
2023-01-13 11:10:17 +0800

    feat: 新增 package，配置 head
    1. 安裝 nuxt/tailwind
    2. 安裝 nuxt/image
    3. 安裝 nuxtjs/svg
    4. 為字體、icon 新增 stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    runtimeConfig: {
+        buildModules: [
+            "@nuxt/image",
+            "@nuxtjs/svg"
+        ],
+    },
+    app: {
+        head: {
+            link: [
+                { rel: "preconnect", href: "https://fonts.googleapis.com" },
+                { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous" },
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Noto+Sans+TC:wght@300;400;500&display=swap" },
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" }
+            ]
+        }
+    }
 })
+
