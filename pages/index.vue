8659907cba1399fee259758eed526a58ea4d8363
sam.liu <sam.liu@axis3d.com>
2023-01-19 17:07:08 +0800

    feat: 新增全局 style.css
    1. 將全局 style 從 homepage 的 <style> tag 移置 style.css

--- pages/index.vue
+++ pages/index.vue
@@ -16,27 +16,3 @@
 <script setup>
 import Three from '../components/Three.vue';
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Noto+Sans+TC:wght@300;400;500&display=swap');
-
-html {
-    height: 100%;
-}
-
-body {
-    font-family: 'Montserrat', 'Noto Sans TC', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: #F5F5F5;
-    height: 100%;
-    margin: 0px;
-}
-
-#__nuxt {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    padding: 0px 24px;
-}
-</style>
