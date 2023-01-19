277e12b7fa1be80656c1f632c552c198c5be2305
sam.liu <sam.liu@axis3d.com>
2023-01-19 17:07:33 +0800

    feat: 更新 default layout
    1. 新增 footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,16 @@
+<!-- default page layout -->
 <template>
-    <header class="pt-5 sm:pt-14 flex justify-between z-10">
-        <!-- default page layout -->
+    <header class="px-[24px] pt-5 sm:pt-14 flex justify-between z-10">
         <img class="sm:w-[281.92px]" src="~assets/svg/logotype.svg" />
         <span class="material-symbols-outlined">menu</span>
     </header>
-    <!-- output page content -->
+
     <main class="grow flex flex-col">
-        <slot />
+        <!-- output page content -->
+        <slot></slot>
     </main>
+
+    <footer class="bg-[#989898] h-[43px] font-['Noto Sans TC'] text-[8px] text-center">
+        Copyright© 愛迪斯科技 2013 Axis 3D Technology, Inc. All Rights Reserved.
+    </footer>
 </template>
