2e0665638daee8b3a5ef365de2a5cf77ef474025
sam.liu <sam.liu@axis3d.com>
2023-01-19 17:05:27 +0800

    feat: 更新 tailwind config
    1.新增字級設定，製作名單 grid-template-columes 設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ module.exports = {
     screens: {
       'sm': '768px'
     },
+    fontSize: {
+      //header
+      h2: '60px',
+      h3: '48px',
+      h4: '34px',
+      h5: '24px',
+      h6: '20px',
+      //subtitle
+      st_lg: '16px', 
+      st_sm: '14px',
+      //body
+      bd_lg: '16px',
+      bd_sm: '14px',
+      //button
+      btn: '14px',
+      //caption
+      cpt: '14px',
+      //overline
+      ovr: '10px'
+    },
+    gridTemplateColumns: {
+      //staff list
+      'staff': '2fr 4fr'
+    },
     extend: {},
   },
   plugins: [],
