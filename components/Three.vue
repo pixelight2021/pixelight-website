09587bcf274a5aca9ac58ccf78716002092814eb
samchl <happysam31640@gmail.com>
2023-01-29 21:01:58 +0800

    feat: 更新 Three component
    1. 更新 Three.vue

--- components/Three.vue
+++ components/Three.vue
@@ -1,7 +1,9 @@
 <template>
     <!-- <div class="relative grow"> -->
-        <canvas id="three-canvas" class="fixed left-0 top-0"></canvas>
-        <span id="pointer" class="fixed z-10 bg-red-400 w-4 h-4 rounded-[50%]"></span>
+    <canvas id="three-canvas" class="fixed left-0 top-0"></canvas>
+    <div id="pointer" @mouseover="hover = true" @mouseleave="hover = false">
+        <nuxt-img v-show="hover" class="rounded-full rotate-[-45deg]" src="jpg/bird.jpg" />
+    </div>
     <!-- </div> -->
 </template>
 
@@ -10,6 +12,8 @@ import { onMounted } from 'vue';
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const hover = ref(false)
 
 onMounted(() => {
     const canvas = document.querySelector('#three-canvas')
@@ -32,7 +36,35 @@ onMounted(() => {
         //onload
         function (gltf) {
             mesh = gltf.scene.children[0]
-            mesh.material = new THREE.MeshBasicMaterial({ color: 'blue' })
+            // mesh.material = new THREE.MeshBasicMaterial({ color: 'blue' })
+            mesh.material = new THREE.ShaderMaterial({
+                // wireframe: true,
+                // normalMap: true,
+                vertexShader: `
+                    varying vec3 vPosition;
+                    varying vec3 vNormal;
+
+                    void main()	{
+                        vec4 result;
+                        vNormal = normal;
+                        vPosition = position;
+                        result = vec4(position, 1.0);
+                        gl_Position = projectionMatrix * modelViewMatrix * result;
+                    }
+                `,
+                fragmentShader: `
+                    #define PI 3.14159265359
+
+                    varying vec3 vPosition;
+                    varying vec3 vNormal;
+
+                    void main() {
+
+                        gl_FragColor = vec4(sin(vPosition.x * PI), 0.0, 0.0, 1.0) * (vec4(1.0, 1.0, 1.0, 1.0) - vec4(abs(vNormal.zzz), 0.0)) + vec4(abs(vNormal.zzz), 1.0);
+                        // gl_FragColor = vec4(abs(vNormal.zzz), 1.0);
+                    }
+                `
+            })
             scene.add(mesh)
         },
         //onProgress
@@ -43,6 +75,10 @@ onMounted(() => {
         });
 
     camera.position.z = 5
+
+    // camera.position.set(0, -3, 1.5)
+    // camera.rotation.x = Math.PI / 2
+
 
     // const workAnchor = new THREE.Vector3(1, 0.85, 0)
     // const contactAnchor = new THREE.Vector3(0, 1.25, 0)
@@ -61,11 +97,13 @@ onMounted(() => {
     }
 
     function updateAnchors() {
-        const aboutAnchor = new THREE.Vector3(-1.5, -0.09, -0.08)
+        const aboutAnchor = new THREE.Vector3(-1.5, -0.17, 0)
+        // const aboutAnchor = new THREE.Vector3(1.06, 0.9, 0)
         aboutAnchor.project(camera)
         const x = Math.round((0.5 + aboutAnchor.x / 2) * (canvas.width));
         const y = Math.round((0.5 - aboutAnchor.y / 2) * (canvas.height));
 
+        // document.getElementById('pointer').style.transform = `translate(-50%, -50%) rotate(45deg)`
         document.getElementById('pointer').style.left = `${x}px`
         document.getElementById('pointer').style.top = `${y}px`
     }
@@ -73,7 +111,39 @@ onMounted(() => {
 </script>
 
 <style>
-/* #three-canvas {
+#pointer {
+    position: fixed;
+    z-index: 1;
+    aspect-ratio: 1/1;
+    transform: translate(-50%, -50%) rotate(45deg);
+    background-color: black;
+
+    transition: width 0.2s ease-in-out, height 0.2s ease-in-out, border-radius 0.2s ease-in-out;
+    width: 5px;
+    border-radius: none;
+}
+
+#pointer:hover {
+    width: 120px;
+    border-radius: 50%;
+}
+
+#pointer::after {
+    content: '';
+    display: block;
     position: absolute;
-} */
+    border: 1px solid black;
+    border-radius: 50%;
+    aspect-ratio: 1/1;
+    width: 24px;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+    transition: all 0.2s ease-in-out;
+}
+
+#pointer:hover::after {
+    width: 140px;
+    border-color: white;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="z-10 text-center">
-            <h4 class="text-[34px]">
-                Pixelight is a Design Team Founded in 2020. We Focus on Creating Interactive Experiences and Digital Art.
-            </h4>
-            <div class="flex justify-center items-center mt-4">
-                <span class="material-symbols-outlined mr-3">360</span>
-                ROTATE TO EXPLORE
-            </div>
+    <div class="relative text-center z-10">
+        <h4 class="text-[34px]">
+            Pixelight is a Design Team Founded in 2020. We Focus on Creating Interactive Experiences and Digital Art.
+        </h4>
+        <div class="flex justify-center items-center mt-4">
+            <span class="material-symbols-outlined mr-3">360</span>
+            ROTATE TO EXPLORE
         </div>
+    </div>
     <ClientOnly>
         <Three />
     </ClientOnly>
 </template>
 
 <script setup>
-import Three from '../components/Three.vue';
+// import Three from '~/components/Three.vue';
 </script>
