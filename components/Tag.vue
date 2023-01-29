203a8b3179dcc55ea8385e9f6055d1534fa755fd
samchl <happysam31640@gmail.com>
2023-01-29 20:52:58 +0800

    feat: 更新專案頁面
    1. 更新 work/[id].vue
    2. 新增 Tag.vue component
    3. 更新 style.css

---
+++ components/Tag.vue
@@ -0,0 +1,11 @@
+<template>
+    <li class="inline-block min-w-[70px] border p-0.5 mr-[10px] text-ovr text-center">
+        #<slot />
+    </li>
+</template>
+
+<!-- <script setup>
+const props = defineProps(['tagId'])
+const appConfig = useAppConfig()
+const tag = appConfig.tags.find(t => t.id === props.tagId)
+</script> -->
--- pages/work/[id].vue
+++ pages/work/[id].vue
@@ -1,50 +1,49 @@
 <template>
-    <div>
-        <nuxt-img class="left-0 w-full" src="http://192.168.2.140/wordpress/wp-content/uploads/2023/01/Intersect.png"></nuxt-img>
-        <div class="absolute">{{ work.name['zh-tw'] }}</div>
-        <!-- <div class="absolute">{{ lang }}</div> -->
+    <div class="relative ">
+        <nuxt-img class="min-w-full h-[438px] object-cover" :src="work.cover.source_url" />
+        <div class="absolute max-w-[90%] bottom-[47px] bg-[#ffffffe6] text-h4 px-[30px] py-[10px]">{{ work.name[`${$i18n.locale}`] }}</div>
     </div>
-    <div class="p-[24px]">
+
+    <div class="py-[24px] p-page">
         <!-- iframe這邊會產生不影響運作的error，待解決 -->
         <iframe class="w-full aspect-video" src="https://player.vimeo.com/video/685312218" allowfullscreen></iframe>
         <div class="flex">
             <div class="block sm:hidden"></div>
-            <p>{{ work.content1 }}</p>
-            <!-- <p>White whale is also called beluga whale. With elegant appearance and pleasant sound, it is also called “canary in the sea”. In the National Museum of Marine Biology and Aquarium (NMMBA), although beluga whales are quite popular among tourists, but the public is still unfamiliar with their physiology and the stories.</p> -->
+            <p>{{ work.content1[`${$i18n.locale}`] }}</p>
         </div>
-        <div>
-            <ul ref="container" class="keen-slider">
-                <li class="keen-slider__slide number-slide1">
-                    {{ work.contnet2 }}
+        <div class="relative">
+            <ul ref="container" class="keen-slider h-[445px]">
+                <li class="keen-slider__slide rounded-[10px] min-w-[255px] ">
+                    {{ work.content2[`${$i18n.locale}`] }}
                 </li>
-                <li class="keen-slider__slide number-slide1">
-                    <nuxt-img src="http://192.168.2.140/wordpress/wp-content/uploads/2023/01/Intersect.png" />
+                <li class="keen-slider__slide rounded-[10px] min-w-[255px]" v-for="media in work.slideshow">
+                    <nuxt-img class="object-cover min-w-full h-full" :src="media.source_url" />
                 </li>
             </ul>
-            <span class="material-symbols-outlined" @click="slider.prev()">chevron_left</span>
-            <span class="material-symbols-outlined" @click="slider.next()">chevron_right</span>
+            <div class="absolute top-1/2 left-[-10px] rounded-full aspect-square w-[30px] bg-[#fff] flex justify-center items-center">
+                <span class="material-symbols-outlined" @click="slider.prev()">west</span>
+            </div>
+            <div class="absolute top-1/2 right-[-10px] rounded-full aspect-square w-[30px] bg-[#fff] flex justify-center items-center">
+                <span class="material-symbols-outlined" @click="slider.next()">east</span>
+            </div>
         </div>
-        <div>{{ work.name.EN }}</div>
+
+        <div>{{ work.name.en }}</div>
+
         <ul class="flex flex-wrap">
-            <li class="tag">#數位藝術</li>
-            <li class="tag">#3D</li>
-            <li class="tag">#展覽</li>
-            <li class="tag">#動畫</li>
-            <li class="tag">#科技</li>
-            <li class="tag">#裝置藝術</li>
+            <Tag v-for="t in work.tags" :tagId="t">{{ t.name }}</Tag>
         </ul>
 
         <div class="grid grid-cols-2 aspect-square">
-            <div>0</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
+            <div class="overflow-hidden" v-for="media in work.gallery">
+                <nuxt-img class="object-cover min-w-full h-full" :src="media.source_url" />
+            </div>
         </div>
 
-        <!-- <div class="grid grid-cols-staff text-ovr align-top" v-for="(value, key) in staff">
+        <div class="grid grid-cols-staff text-ovr align-top" v-for="(value, key) in work.staff[`${$i18n.locale}`]">
             <span>{{ key }}</span>
             <span>{{ value }}</span>
-        </div> -->
+        </div>
 
         <div class="flex justify-between">
             <button>上個專案</button>
@@ -54,58 +53,56 @@
 </template>
 
 <script setup>
-import { computed, ref, reactive } from 'vue'
-
 import { useKeenSlider } from 'keen-slider/vue.es'
 import 'keen-slider/keen-slider.min.css'
-const { locale } = useI18n()
-const lang = computed(() => locale.name)
-console.log(locale)
 
-const current = ref(0)
 const [container, slider] = useKeenSlider({
-    initial: current.value,
-    slideChanged: (s) => {
-        current.value = s.track.details.rel
-    },
+    slides: {
+        origin: 'center',
+        perView: 'auto',
+        spacing: 30
+    }
 })
 
+const appConfig = useAppConfig()
 let work
 
 try {
-    const { server_url } = useRuntimeConfig()
     const { id } = useRoute().params
-    const url = server_url + '/wp-json/wp/v2/work/' + id
-    const { data } = await useFetch(url)
+    const url = 'http://localhost/wordpress/wp-json/wp/v2/work/' + id
+    const {data} = await useFetch(url)
     if (!data?.value) throw new Error('work not found')
 
     work = reactive({
         name: data.value.acf.name,
-        cover: data.value.acf.cover,
+        cover: computed(() => appConfig.medias.find(m => m.id === data.value.acf.cover)),
         trailer: data.value.acf.trailer,
         content1: data.value.acf.content1,
-        slideshow: data.value.acf.slideshow,
+        slideshow: computed(() => {
+            const keys = Object.keys(data.value.acf.slideshow).filter(k => data.value.acf.slideshow[k] != '')
+            const medias = keys.map(k => appConfig.medias.find(m => m.id === data.value.acf.slideshow[k]))
+            return medias
+        }),
         content2: data.value.acf.content2,
-        gallery: data.value.acf.gallery,
-        staff: data.value.acf.staff
+        tags: computed(() => data.value.tags.map(id => appConfig.tags.find(t => t.id === id))),
+        gallery: computed(() => {
+            const keys = Object.keys(data.value.acf.gallery)
+            const medias = keys.map(k => appConfig.medias.find(m => m.id === data.value.acf.gallery[k]))
+            return medias
+        }),
+        staff: computed(() => {
+            const langs = Object.keys(data.value.acf.staff)
+            const staff = langs.reduce((accumulator, lang) => {
+                const keys = Object.keys(data.value.acf.staff[lang]).filter(k => data.value.acf.staff[lang][k] != '')
+                const members = keys.reduce((a, k) => ({ ...a, [k]: data.value.acf.staff[lang][k] }), {})
+                return { ...accumulator, [lang]: members }
+            }, {})
+            return staff
+        })
     })
 } catch (error) {
     console.error(error)
     //redirect to home page if work not found
     useRouter().push('/')
 }
-
-// const staff = computed(() => {
-//     let o = Object.keys(work.acf.staff)
-//         .filter(k => work.acf.staff[k] != '')
-//         .reduce((a, k) => ({ ...a, [k]: work.acf.staff[k] }), {})
-//     console.log(o)
-//     return o
-// })
 </script>
-
-<style scope>
-.tag {
-    @apply inline min-w-[70px] text-ovr text-center text-[#00000060] border border-[#00000060] p-0.5 mr-[10px]
-}
-</style>
