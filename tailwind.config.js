31792ff59ccd326da6af7f30f62677e758d1fd77
sam.liu <sam.liu@axis3d.com>
2023-02-03 14:17:22 +0800

    feat: 更新 tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  important: true,
   theme: {
     screens: {
       'sm': '768px'
     },
-    fontSize: {
-      //header
-      h2: '60px',
-      h3: '48px',
-      h4: '34px',
-      h5: '24px',
-      h6: '20px',
-      //subtitle
-      'st-lg': '16px',
-      'st-sm': '14px',
-      //body
-      bd_lg: '16px',
-      bd_sm: '14px',
-      //button
-      btn: '14px',
-      //caption
-      cpt: '14px',
-      //overline
-      ovr: '10px'
-    },
     extend: {
+      fontSize: {
+        //header
+        h2: '60px',
+        h3: '48px',
+        h4: '34px',
+        h5: '24px',
+        h6: '20px',
+        //subtitle
+        'st-lg': '16px',
+        'st-sm': '14px',
+        //body
+        'bg-lg': '16px',
+        'bd-sm': '14px',
+        //button
+        btn: '14px',
+        //caption
+        cpt: '14px',
+        //overline
+        ovr: '10px'
+      },
       padding: {
         'page': '0 24px'
       },
       gridTemplateColumns: {
+        'input': '1fr 5fr',
         //staff list
         'staff': '2fr 4fr'
       }
